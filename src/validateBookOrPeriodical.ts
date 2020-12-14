1afd7707698b4a88cc407c89afd018d48eb8118b
gabrielSossai <biel.sossai@outlook.com>
2020-12-13 21:43:25 -0300

    Finalização dos exercicios

--- src/validateBookOrPeriodical.ts
+++ src/validateBookOrPeriodical.ts
@@ -1,6 +1,6 @@
 import Book from './entities/Book.js'
 import Periodical from './entities/Periodical.js'
-import {showPeople} from './validatePerson.js'
+import { showPeople } from './validatePerson.js'
 import Person from './entities/Person.js'
 
 const title = document.querySelector<HTMLInputElement>('#title')!
@@ -8,7 +8,7 @@ const subtitle = document.querySelector<HTMLInputElement>('#subtitle')!
 const publishedAt = document.querySelector<HTMLInputElement>('#publishedAt')!
 const author = document.querySelector<HTMLSelectElement>('#author')!
 const isbn = document.querySelector<HTMLInputElement>('#isbn')!
-const edition = document.querySelector<HTMLInputElement>('#edition')!
+const edicao = document.querySelector<HTMLInputElement>('#edition')!
 const volume = document.querySelector<HTMLInputElement>('#volume')!
 const issn = document.querySelector<HTMLInputElement>('#issn')!
 const issue = document.querySelector<HTMLInputElement>('#issue')!
@@ -16,6 +16,8 @@ const message = document.querySelector<HTMLParagraphElement>('#message')!
 const formulario = document.querySelector<HTMLFormElement>('form')!
 const selectType = document.querySelector<HTMLSelectElement>('#type')!
 const authorName = document.querySelector<HTMLSelectElement>('#authorName')!
+const table = document.querySelector<HTMLTableCaptionElement>('#table')!
+const tableBooks = document.querySelector<HTMLTableCaptionElement>('#tableBooks')!
 
 const Periodicals: Periodical[] = []
 showPeriodicals()
@@ -25,13 +27,14 @@ showBooks()
 
 selectType.addEventListener('change', (ev: Event) => {
     ev.preventDefault()
+
     if (selectType.value == "l") {
         title.style.display = "block";
         subtitle.style.display = "block";
         publishedAt.style.display = "block";
         author.style.display = "block";
         isbn.style.display = "block";
-        edition.style.display = "block";
+        edicao.style.display = "block";
         volume.style.display = "block";
         issue.style.display = "none";
         issn.style.display = "none";
@@ -45,10 +48,10 @@ selectType.addEventListener('change', (ev: Event) => {
         publishedAt.style.display = "block";
         author.style.display = "block";
         issue.style.display = "block";
-        issn.style.display =  '';
+        issn.style.display = '';
         volume.style.display = "block";
         isbn.style.display = "none";
-        edition.style.display = "none";
+        edicao.style.display = "none";
         pullNames()
 
     } else {
@@ -61,89 +64,115 @@ selectType.addEventListener('change', (ev: Event) => {
         issn.style.display = "none";
         volume.style.display = "none";
         isbn.style.display = "none";
-        edition.style.display = "none";
+        edicao.style.display = "none";
     }
 })
 
-let personLocalStorage: Array<Person> = JSON.parse(localStorage.getItem('People')||'{}')
-let varnames = personLocalStorage.map (p=> p.name)
+let personLocalStorage: Array<Person> = JSON.parse(localStorage.getItem('People') || '{}')
+let varnames = personLocalStorage.map(p => p.name)
 
-    formulario.addEventListener('submit', (e: Event) => {
-        e.preventDefault()
+formulario.addEventListener('submit', (e: Event) => {
+    e.preventDefault()
 
-        var person = personLocalStorage[parseInt(author.value)]
+    const capitalize = (text: string) => {
+        const words = text.split(' ')
 
-        if (!title.value.trim()) {
-            message.innerText = 'O campo Título é obrigatório!'
-            title.focus()
-            return
+        for (let i = 0; i < words.length; i++) {
+            words[i] = words[i].substr(0, 1).toUpperCase() + words[i].substr(1).toLowerCase()
         }
-    
-        if (!subtitle.value.trim()) {
-            message.innerText = 'O campo Subtítulo é obrigatório!'
-            title.focus()
-            return
-        }
-    
-        if (!publishedAt.value) {
-            message.innerText = 'Este campo é obrigatório!'
-            publishedAt.focus()
-            return
-        }
-    
-        const publishedDate = new Date (`^${publishedAt.value}T00:00:00`)
-    
-        if (Date.now() - Number(publishedDate) < 0) {
-            message.innerText = 'Escolha uma data do passado!'
-            publishedAt.focus()
-            return
-        }
-    
-        const authorValue = author.value.trim()
-        if (!authorValue) {
-            message.innerText = 'O campo Autor é obrigatório'
-            author.focus()
-            return
-        }
-    
-        //const regexNome = /\w+\s\w+/g
-    
-        /* if (!regexNome.test(authorValue)) {
-            message.innerText = 'Informe o nome completo do autor!'
-            author.focus()
-            return
-        } */
-    
-        if (selectType.value == 'l') {
+        return words.join(' ')
+            .replace(/ e /gi, ' e ')
+            .replace(/ do /gi, ' do ')
+            .replace(/ dos /gi, ' dos ')
+            .replace(/ das /gi, ' das ')
+            .replace(/ de /gi, ' de ')
+            .replace(/ das /gi, ' das ')
+            .replace(/ a /gi, ' a ')
+            .replace(/ o /gi, ' o ')
+            .replace(/ as /gi, ' as ')
+            .replace(/ no /gi, ' no ')
+            .replace(/ nos /gi, ' nos ')
+            .replace(/ na /gi, ' na ')
+            .replace(/ nas /gi, ' nas ')
+    }
+
+    const trimAll = (text: string) => text.trim().replace(/\s+/g, ' ')
+
+    var person = personLocalStorage[parseInt(author.value)]
+
+    if (!title.value.trim()) {
+        message.innerText = 'O campo Título é obrigatório!'
+        title.focus()
+        return
+    }
+
+    if (!subtitle.value.trim()) {
+        message.innerText = 'O campo Subtítulo é obrigatório!'
+        title.focus()
+        return
+    }
+
+    if (!publishedAt.value) {
+        message.innerText = 'O campo data de publicação é obrigatório!'
+        publishedAt.focus()
+        return
+    }
+
+    const publishedDate = new Date(`^${publishedAt.value}T00:00:00`)
+
+    if (Date.now() - Number(publishedDate) < 0) {
+        message.innerText = 'Escolha uma data do passado!'
+        publishedAt.focus()
+        return
+    }
+
+    const authorValue = author.value.trim()
+    if (!authorValue) {
+        message.innerText = 'O campo Autor é obrigatório'
+        author.focus()
+        return
+    }
+
+    //const regexNome = /\w+\s\w+/g
+
+    /* if (!regexNome.test(authorValue)) {
+        message.innerText = 'Informe o nome completo do autor!'
+        author.focus()
+        return
+    } */
+
+    if (selectType.value == 'l') {
         if (!isbn.value) {
             message.innerText = 'O campo ISBN é obrigatório'
             isbn.focus()
             return
         }
-        }
-    
-        if (!edition.value.trim()) {
-            message.innerText = 'O campo Edição é obrigatório'
-            edition.focus()
+    }
+
+   if (selectType.value == 'l')
+        if (!edicao.value) {
+            message.innerText = 'AAAAAAAAAA FILHO DA PUTAAAAAAAAA'
+            edicao.focus()
             return
         }
-    
-        if (!volume.value) {
-            message.innerText = 'O campo Volume é obrigatório'
-            volume.focus()
-            return
-        }
-        
-    
-        if (selectType.value == 'p') { 
+
+
+    if (!volume.value) {
+        message.innerText = 'O campo Volume é obrigatório'
+        volume.focus()
+        return
+    }
+
+
+    if (selectType.value == 'p') {
         if (!issn.value) {
             message.innerText = 'O campo ISSN é obrigatório'
             issn.focus()
             return
-        } 
+        }
     }
-    
-        if (selectType.value == 'p') {
+
+    if (selectType.value == 'p') {
         if (!issue.value.trim()) {
             message.innerText = 'O campo Issue é obrigatório'
             issue.focus()
@@ -151,41 +180,41 @@ let varnames = personLocalStorage.map (p=> p.name)
         }
     }
     message.innerText = 'Seu cadastro foi realizado com sucesso!'
-    
+
     if (selectType.value == 'l') {
-    try {
-        let publishDate = new Date (publishedAt.value)
-        let newISBN : number = +isbn.value
-        let newEdition : number = +edition.value
-        let newVolume : number = +volume.value
-        const book = new Book(title.value, subtitle.value, publishDate, person, newISBN, newEdition, newVolume)
-        Books.push(book)
-        localStorage.setItem('Books', JSON.stringify(Books))
-        showBooks()
-    }
-    catch (error: any){
-        message.innerText = 'Algo de errado não está certo!'
-        return
-    }
-    message.innerText = 'Seu cadastro foi realizado com sucesso!'
+        try {
+            let publishDate = new Date(publishedAt.value)
+            let newISBN: number = +isbn.value
+            let newEdition: number = +edicao.value
+            let newVolume: number = +volume.value
+            const book = new Book(capitalize(trimAll(title.value)), subtitle.value, publishDate, person, newISBN, newEdition, newVolume)
+            Books.push(book)
+            localStorage.setItem('Books', JSON.stringify(Books))
+            showBooks()
+        }
+        catch (error: any) {
+            message.innerText = 'Algo de errado não está certo!'
+            return
+        }
+        message.innerText = 'Seu cadastro foi realizado com sucesso!'
     }
 
     if (selectType.value == 'p') {
-    try {
-        let publishDate = new Date (publishedAt.value)
-        let newISSN : number = +issn.value
-        let newVolume : number = +volume.value
-        let newIssue : number = + issue.value
-        const periodical = new Periodical(newISSN, newVolume, newIssue, title.value, subtitle.value, publishDate, person)
+        try {
+            let publishDate = new Date(publishedAt.value)
+            let newISSN: number = +issn.value
+            let newVolume: number = +volume.value
+            let newIssue: number = + issue.value
+            const periodical = new Periodical(newISSN, newVolume, newIssue, capitalize(trimAll(title.value)), subtitle.value, publishDate, person);
 
-        Periodicals.push(periodical)
-        localStorage.setItem('Periodicals', JSON.stringify(Periodicals))
-        showPeriodicals()
-    } catch (error: any) {
-        message.innerText = 'Algo de errado não está certo!'
-        return
-    }
-    message.innerText = 'Seu cadastro foi realizado com sucesso!'
+            Periodicals.push(periodical)
+            localStorage.setItem('Periodicals', JSON.stringify(Periodicals))
+            showPeriodicals()
+        } catch (error: any) {
+            message.innerText = 'Algo de errado não está certo!'
+            return
+        }
+        message.innerText = 'Seu cadastro foi realizado com sucesso!'
     }
 
 })
@@ -215,11 +244,59 @@ function showPeriodicals() {
 }
 
 function pullNames() {
-    for (let i = 0; i < varnames.length;i++) {
+    for (let i = 0; i < varnames.length; i++) {
         author.add(new Option(varnames[i].toString(), i.toString()))
     }
 }
 
+let periodicalLocalStorage: Array<Periodical> = JSON.parse(localStorage.getItem('Periodicals') || '{}')
+let periodicalTitles = periodicalLocalStorage.map (p => p.title) 
+let sortPeriodicals = [...periodicalTitles].sort()
 
+let lines = ' '
 
+for (let i = 0; i < sortPeriodicals.length; i++) {
+    lines += `
+    <tr>
+    <td>${sortPeriodicals[i]}</td>
+    </tr>
+    `
+}
 
+table.style.display = 'block'
+table.innerHTML = `
+    <thread>
+        <tr>
+        Periódicos:
+        </tr
+    </thread>
+    <tbody>
+        ${lines}
+    </tbody>
+` 
+
+let bookLocalStorage: Array<Book> = JSON.parse(localStorage.getItem('Books') || '{}')
+let bookTitles = bookLocalStorage.map (p => p.title) 
+let sortBooks = [...bookTitles].sort()
+
+let linesBooks = ' '
+
+for (let i = 0; i < sortBooks.length; i++) {
+    linesBooks += `
+    <tr>
+    <td>${sortBooks[i]}</td>
+    </tr>
+    `
+}
+
+tableBooks.style.display = 'block'
+tableBooks.innerHTML = `
+    <thread>
+        <tr>
+        Livros:
+        </tr
+    </thread>
+    <tbody>
+        ${linesBooks}
+    </tbody>
+` 
--- src/validatePerson.ts
+++ src/validatePerson.ts
@@ -6,16 +6,34 @@ const birth = document.querySelector<HTMLInputElement>('#birth')!
 const gender = document.querySelector<HTMLSelectElement>('#gender')!
 const message = document.querySelector<HTMLParagraphElement>('#message')!
 const formulario = document.querySelector<HTMLFormElement>('form')!
+const verify = document.querySelector<HTMLButtonElement>('#verify')!
+const search = document.querySelector<HTMLInputElement>('#search')!
+//const filter = document.querySelector<HTMLButtonElement>('#filter')!
+
 
 const People: Person[] = []
 showPeople()
 
 
-formulario.addEventListener('submit', (e: Event) => {
+verify.addEventListener('click', (e: Event) => {
     e.preventDefault()
+    const capitalize = (text: string) => {
+        const words = text.split(' ')
 
+        for (let i = 0; i < words.length; i++) {
+            words[i] = words[i].substr(0, 1).toUpperCase() + words[i].substr(1).toLowerCase()
+        }
+        return words.join(' ')
+            .replace(/ e /gi, ' e ')
+            .replace(/ do /gi, ' do ')
+            .replace(/ dos /gi, ' dos ')
+            .replace(/ das /gi, ' das ')
+            .replace(/ de /gi, ' de ')
+    }
 
-    const nameValue = name.value.trim()
+    const trimAll = (text: string) => text.trim().replace(/\s+/g, ' ')
+
+    let nameValue = name.value.trim()
 
     if (!nameValue) {
         message.innerText = 'O campo Nome é obrigatório'
@@ -23,13 +41,13 @@ formulario.addEventListener('submit', (e: Event) => {
         return
     }
 
-    const regexNome = /\w+\s\w+/g
+    /* const regexNome = /\w+\s\w+/g
 
-    if (!regexNome.test(nameValue)) {
+    if (!regexNome.test(name.value)) {
         message.innerText = 'Informe seu Nome completo!'
         name.focus()
         return
-    }
+    } */
 
     if (!birth.value) {
         message.innerText = 'O campo Nascimento é obrigatório!'
@@ -50,28 +68,26 @@ formulario.addEventListener('submit', (e: Event) => {
         gender.focus()
         return
     }
-    
+
     try {
         let birthNew = new Date(birth.value)
 
-        const person = new Person(name.value, birthNew, gender.value === 'f' ? Gender.Female : Gender.Male)
+        const person = new Person(capitalize(trimAll((name.value))), birthNew, gender.value === 'f' ? Gender.Female : Gender.Male)
 
         People.push(person)
         localStorage.setItem('People', JSON.stringify(People))
         showPeople()
     }
 
-    
-
     catch (error: any) {
         message.innerText = 'DEU PAU!'
         return
     }
-    
+
     message.innerText = 'Cadastro realizado com sucesso!'
 })
 
-export function showPeople()  {
+export function showPeople() {
     if (localStorage.getItem('People')) {
         const data = JSON.parse(localStorage.getItem('People')!)
 
@@ -83,3 +99,56 @@ export function showPeople()  {
     }
 }
 
+//Pega os dados do Local Storage
+let personLocalStorage: Array<Person> = JSON.parse(localStorage.getItem('People') || '{}')
+//Seleciona os nomes no local storage
+let varnames = personLocalStorage.map(p => p.name)
+//Ordena os nomes por ordem alfabética
+let sortnames = [...varnames].sort()
+
+let varbirth = personLocalStorage.map(p => p.birth)
+//let parsebirth = math.round(varbirth)
+let vargender = personLocalStorage.map(p => p.gender)
+
+let lines = ' '
+let table = document.querySelector<HTMLTableCaptionElement>('#table')!
+
+for (let i = 0; i < sortnames.length; i++) {
+    lines += `
+        <tr>
+        <td>${sortnames[i]}</td>
+        </tr>
+        `
+}
+
+table.style.display = 'block'
+table.innerHTML = `
+  <thread>
+    <tr> 
+        Autores:
+    </tr>
+  </thread>
+  <tbody>
+    ${lines}
+    </tbody>
+  `
+
+  search.addEventListener("keyup", filter)
+  function filter() {
+      if (search.value) {
+        let peopleLocalStorage: Array<Person> = JSON.parse(localStorage.getItem("people")!)
+
+        const onlyPeople = (obj: typeof peopleLocalStorage[0]) => obj.name.includes(search.value)
+
+        let filtrar = peopleLocalStorage.filter(onlyPeople)
+        let lines = ' '
+        for (const people of filtrar) {
+            lines += `
+            <tr>
+                <td>${(people as Person).name}</td>
+            </tr>
+            `
+        }
+      }
+  }
+    
